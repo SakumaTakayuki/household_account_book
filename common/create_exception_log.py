8e5a43cecf3c903fe2f5ecfaafeb704c907b971e
SakumaTakayuki <114669952+SakumaTakayuki@users.noreply.github.com>
2025-05-25 21:53:45 +0900

    【create_exception_log】作成ファイル名変更

--- common/create_exception_log.py
+++ common/create_exception_log.py
@@ -5,7 +5,7 @@ from zoneinfo import ZoneInfo
 class Create_Exception_Log:
     def create_exception_log(self, arg_function_id, arg_log_detail):
         try:
-            f = open("log/Create_Exception_Log_Test.txt", "a+", encoding="utf-8")
+            f = open("log/exception_log.txt", "a+", encoding="utf-8")
             f.write(
                 f"{datetime.now(ZoneInfo("Asia/Tokyo"))}  {arg_function_id}   {arg_log_detail}\n"
             )
