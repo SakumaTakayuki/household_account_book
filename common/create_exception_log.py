31edc92fc204112df3cffdc11a29b0625894aab4
SakumaTakayuki <114669952+SakumaTakayuki@users.noreply.github.com>
2025-05-28 00:10:39 +0900

    【create_exception_log】作成

--- common/create_exception_log.py
+++ common/create_exception_log.py
@@ -12,6 +12,7 @@ class Create_Exception_Log:
     def create_exception_log(self, arg_function_id, arg_log_detail):
         f = None
         try:
+<<<<<<< HEAD
             # 規定フォルダにファイルがない場合は作成して開く
             # ファイルがある場合はカーソルが最下行の状態で開く
             f = open(
@@ -19,6 +20,11 @@ class Create_Exception_Log:
                 "a+",
                 encoding="utf-8",
             )
+=======
+            # log/exception_log.txtにファイルがない場合は作成して開く
+            # ファイルがある場合はカーソルが最下行の状態で開く
+            f = open("log/exception_log.txt", "a+", encoding="utf-8")
+>>>>>>> af26fde (【create_exception_log】作成)
             # 「現在時間   処理ID  例外エラー内容」を書き込む
             f.write(
                 f"{datetime.now(ZoneInfo("Asia/Tokyo"))}  {arg_function_id}   {arg_log_detail}\n"
@@ -27,7 +33,12 @@ class Create_Exception_Log:
         except:
             return True
         finally:
+<<<<<<< HEAD
             # ファイルが開かれている場合
             if f is not None:
                 # ファイルを閉じる
                 f.close()
+=======
+            # ファイルを閉じる
+            f.close()
+>>>>>>> af26fde (【create_exception_log】作成)
