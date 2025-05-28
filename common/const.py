63f2cd80c188078a67b238d4346d9efaf5e0bde5
SakumaTakayuki <114669952+SakumaTakayuki@users.noreply.github.com>
2025-05-28 23:37:01 +0900

    【const】作成

--- common/const.py
+++ common/const.py
@@ -1,5 +1,11 @@
+# 定数
 class Const:
+    # ファイルパス
+    class FilePath:
+        EXCEPTION_FOLDER = "log"
+        EXCEPTION_FILE = "exception_log.txt"
 
+    # ログ用種類
     class Log_Kinds:
         START = "S"
         END = "E"
@@ -7,23 +13,18 @@ class Const:
         CAUTION = "C"
         WARNING = "W"
 
+    # ログ用処理
     class Log_Process:
         SELECT = "SEL"
         INSERT = "INS"
         UPDATE = "UPD"
         DELETE = "DEL"
 
+    # ログ用機能
     class Log_Function:
         HAB = "HAB"
         USERS = "USERS"
 
-    class Admin:
-        USER_ID = "admin"
-        NAME = "admin"
-        PAAWORD = "admin"
-
-    class Table_Name:
-        USERS = "ユーザー情報"
-
+    # 文字
     class Const_Text:
         TEXT_BLANK = ""
