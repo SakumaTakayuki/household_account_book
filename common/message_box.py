896c022d06a2d297d00447f4a8677347e094ecaa
SakumaTakayuki <114669952+SakumaTakayuki@users.noreply.github.com>
2025-05-28 23:37:36 +0900

    【message_box】作成

--- common/message_box.py
+++ common/message_box.py
@@ -1,4 +1,7 @@
+# 画面用メッセージボックス
 class Message_Box:
     def __init__(self):
+        # 画面のメッセージボックスに表示するIDを代入する
         self.message_id = None
+        # 画面のメッセージボックスに表示するメッセージを代入する
         self.message_text = None
