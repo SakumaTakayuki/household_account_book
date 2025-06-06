66db877f5bd49976a57ae452449f25ce769b5124
SakumaTakayuki <114669952+SakumaTakayuki@users.noreply.github.com>
2025-06-06 22:07:10 +0900

    【my_control】表作成

--- common/my_control.py
+++ common/my_control.py
@@ -4,12 +4,18 @@ import flet as ft
 class My_Control:
 
     class HAB_LIST:
-        def __init__(self, testpage):
-            self.page = testpage
+        def __init__(self, arg_page):
+            self.page = arg_page
             self.control = None
-            row_button_width = 50
-            row_button_height = 50
-            row_width = 100
+
+        def set_data_list(self, arg_column_name, arg_data_list):
+            self.control = None
+            column_name = arg_column_name
+            data_list = arg_data_list
+            row_button_width = 40
+            row_button_height = 40
+            column_width = 200
+            row_width = 199.95
             row_height = 40
             topfirst_row_border_config = ft.border.all(2.0, ft.Colors.BLACK)
             topinside_row_border_config = ft.border.only(
@@ -44,98 +50,44 @@ class My_Control:
                 bottom=ft.border.BorderSide(2.0, ft.Colors.BLACK),
             )
             # 列名作成
-            i = 999
-            columu_title = ft.Row(
-                [
-                    ft.Container(
-                        width=50,
-                        height=50,
-                        bgcolor=ft.Colors.with_opacity(
-                            0.0, ft.Colors.PRIMARY_CONTAINER
-                        ),
-                        border=topfirst_row_border_config,
-                        data=i,
-                    ),
-                    ft.Container(
-                        content=ft.Text(
-                            value=f"利用日時",
-                            weight=ft.FontWeight.BOLD,
-                        ),
-                        width=200,
-                        height=50,
-                        padding=ft.padding.only(left=5),
-                        alignment=ft.alignment.center_left,
-                        bgcolor=ft.Colors.with_opacity(
-                            0.0, ft.Colors.PRIMARY_CONTAINER
-                        ),
-                        border=topinside_row_border_config,
-                        data=i,
-                        on_click=lambda e: My_Control.HAB_LIST.on_child_click(
-                            self.page,
-                            e,
-                            testConlist,
-                            ft.Colors.RED_ACCENT_100,
-                        ),
-                    ),
-                    ft.Container(
-                        content=ft.Text(value=f"入出金区分", weight=ft.FontWeight.BOLD),
-                        width=200,
-                        height=50,
-                        alignment=ft.alignment.center_left,
-                        bgcolor=ft.Colors.with_opacity(
-                            0.0, ft.Colors.PRIMARY_CONTAINER
-                        ),
-                        border=topinside_row_border_config,
-                        data=i,
-                        on_click=lambda e: My_Control.HAB_LIST.on_child_click(
-                            self.page,
-                            e,
-                            testConlist,
-                            ft.Colors.RED_ACCENT_100,
-                        ),
-                    ),
-                    ft.Container(
-                        content=ft.Text(value=f"金額", weight=ft.FontWeight.BOLD),
-                        width=200,
-                        height=50,
-                        alignment=ft.alignment.center_left,
-                        bgcolor=ft.Colors.with_opacity(
-                            0.0, ft.Colors.PRIMARY_CONTAINER
-                        ),
-                        border=topinside_row_border_config,
-                        data=i,
-                        on_click=lambda e: My_Control.HAB_LIST.on_child_click(
-                            self.page,
-                            e,
-                            testConlist,
-                            ft.Colors.RED_ACCENT_100,
-                        ),
-                    ),
-                    ft.Container(
-                        content=ft.Text(value=f"詳細種類", weight=ft.FontWeight.BOLD),
-                        width=200,
-                        height=50,
-                        alignment=ft.alignment.center_left,
-                        bgcolor=ft.Colors.with_opacity(
-                            0.0, ft.Colors.PRIMARY_CONTAINER
-                        ),
-                        border=topinside_row_border_config,
-                        data=i,
-                        on_click=lambda e: My_Control.HAB_LIST.on_child_click(
-                            self.page,
-                            e,
-                            testConlist,
-                            ft.Colors.RED_ACCENT_100,
-                        ),
-                    ),
-                ],
-                spacing=0,
+            column_row = []
+            column_data = 0
+            column_row_0 = ft.Container(
+                width=row_button_width,
+                height=row_button_height,
+                bgcolor=ft.Colors.with_opacity(0.0, ft.Colors.PRIMARY_CONTAINER),
+                border=topfirst_row_border_config,
+                data=column_data,
             )
+            column_row.append(column_row_0)
+            for name in column_name:
+                column_data = column_data + 1
+                column = ft.Container(
+                    content=ft.Text(
+                        value=name,
+                        weight=ft.FontWeight.BOLD,
+                    ),
+                    width=column_width,
+                    height=row_height,
+                    padding=ft.padding.only(left=5),
+                    alignment=ft.alignment.center_left,
+                    bgcolor=ft.Colors.with_opacity(0.0, ft.Colors.PRIMARY_CONTAINER),
+                    border=topinside_row_border_config,
+                    data=column_data,
+                    on_click=lambda e: My_Control.HAB_LIST.on_child_click(
+                        self.page,
+                        e,
+                        cont_list,
+                        ft.Colors.RED_ACCENT_100,
+                    ),
+                )
+                column_row.append(column)
+            columu_name = ft.Row(controls=column_row, spacing=0)
             # 行作成
-            testlist = []
-            row_count = 50
-            for i in range(0, row_count):
-                if i == row_count - 1:
+            detail_row = []
+            row_data = 0
+            for data in data_list:
+                if row_data == len(data_list) - 1:
                     first_row_border_config = endfirst_row_border_config
                     inside_row_border_config = endinside_row_border_config
                     end_row_border_config = endend_row_border_config
@@ -143,168 +95,107 @@ class My_Control:
                     first_row_border_config = insidefirst_row_border_config
                     inside_row_border_config = insideinside_row_border_config
                     end_row_border_config = insideend_row_border_config
-                test = ft.Row(
-                    [
-                        ft.Container(
-                            ft.Container(
-                                content=ft.TextButton(
-                                    content=ft.Text(
-                                        "詳",
-                                        size=12,
-                                        color=ft.Colors.BLACK,
-                                        text_align=ft.TextAlign.CENTER,
-                                    ),
-                                    data=i,
-                                    on_click=lambda e: My_Control.HAB_LIST.page_go(
-                                        self.page,
-                                        e,
-                                        testConlist,
-                                        ft.Colors.RED_ACCENT_100,
-                                    ),
-                                    expand=True,
-                                ),
-                                alignment=ft.alignment.center,
-                                bgcolor=ft.Colors.GREY,
-                                border=ft.border.all(1.0),
-                                border_radius=10,
+                row_cont_list = []
+                row_cont_0 = ft.Container(
+                    ft.Container(
+                        content=ft.TextButton(
+                            content=ft.Text(
+                                "詳",
+                                size=12,
+                                color=ft.Colors.BLACK,
+                                text_align=ft.TextAlign.CENTER,
                             ),
-                            width=50,
-                            height=50,
-                            bgcolor=ft.Colors.with_opacity(
-                                0.0, ft.Colors.PRIMARY_CONTAINER
-                            ),
-                            border=first_row_border_config,
-                            data=i,
-                        ),
-                        ft.Container(
-                            content=ft.Text(f"testB{i}"),
-                            width=199.5,
-                            height=50,
-                            bgcolor=ft.Colors.with_opacity(
-                                0.0, ft.Colors.PRIMARY_CONTAINER
-                            ),
-                            border=inside_row_border_config,
-                            data=i,
-                            on_click=lambda e: My_Control.HAB_LIST.on_child_click(
+                            data=row_data,
+                            on_click=lambda e: My_Control.HAB_LIST.page_go(
                                 self.page,
                                 e,
-                                testConlist,
-                                ft.Colors.RED_ACCENT_100,
+                                cont_list,
                             ),
+                            expand=True,
                         ),
-                        ft.Container(
-                            content=ft.Text(f"testC{i}"),
-                            width=200,
-                            height=50,
-                            padding=10,
-                            bgcolor=ft.Colors.with_opacity(
-                                0.0, ft.Colors.PRIMARY_CONTAINER
-                            ),
-                            border=inside_row_border_config,
-                            data=i,
-                            on_click=lambda e: My_Control.HAB_LIST.on_child_click(
-                                self.page,
-                                e,
-                                testConlist,
-                                ft.Colors.RED_ACCENT_100,
-                            ),
-                        ),
-                        ft.Container(
-                            content=ft.Text(f"testD{i}"),
-                            width=200,
-                            height=50,
-                            padding=10,
-                            bgcolor=ft.Colors.with_opacity(
-                                0.0, ft.Colors.PRIMARY_CONTAINER
-                            ),
-                            border=inside_row_border_config,
-                            data=i,
-                            on_click=lambda e: My_Control.HAB_LIST.on_child_click(
-                                self.page,
-                                e,
-                                testConlist,
-                                ft.Colors.RED_ACCENT_100,
-                            ),
-                        ),
-                        ft.Container(
-                            content=ft.Text(f"testE{i}"),
-                            width=200,
-                            height=50,
-                            padding=10,
-                            bgcolor=ft.Colors.with_opacity(
-                                0.0, ft.Colors.PRIMARY_CONTAINER
-                            ),
-                            border=end_row_border_config,
-                            data=i,
-                            on_click=lambda e: My_Control.HAB_LIST.on_child_click(
-                                self.page,
-                                e,
-                                testConlist,
-                                ft.Colors.RED_ACCENT_100,
-                            ),
-                        ),
-                    ],
-                    spacing=0,
+                        alignment=ft.alignment.center,
+                        bgcolor=ft.Colors.GREY,
+                        border=ft.border.all(1.0),
+                        border_radius=10,
+                    ),
+                    width=row_button_width,
+                    height=row_button_height,
+                    bgcolor=ft.Colors.with_opacity(0.0, ft.Colors.PRIMARY_CONTAINER),
+                    border=first_row_border_config,
+                    data=row_data,
                 )
-                testlist.append(test)
-            testConlist = []
-            testCon0 = ft.Container()
-            testCon0.content = columu_title
-            testCon0.width = (
-                columu_title.controls[0].width
-                + columu_title.controls[1].width
-                + columu_title.controls[2].width
-                + columu_title.controls[3].width
-                + columu_title.controls[4].width
-            )
-            testConlist.append(testCon0)
+                row_cont_list.append(row_cont_0)
+                index = 0
+                for row_detail in data:
+                    row_cont = ft.Container(
+                        content=ft.Text(row_detail),
+                        width=row_width,
+                        height=row_height,
+                        padding=ft.padding.only(left=5),
+                        alignment=ft.alignment.center_left,
+                        bgcolor=ft.Colors.with_opacity(
+                            0.0, ft.Colors.PRIMARY_CONTAINER
+                        ),
+                        data=row_data,
+                        on_click=lambda e: My_Control.HAB_LIST.on_child_click(
+                            self.page,
+                            e,
+                            cont_list,
+                        ),
+                    )
+                    if index == len(column_name) - 1:
+                        row_cont.border = end_row_border_config
+                    else:
+                        row_cont.border = inside_row_border_config
+                    row_cont_list.append(row_cont)
+                    index = index + 1
+                detail_row.append(ft.Row(controls=row_cont_list, spacing=0))
+                row_data = row_data + 1
+            cont_list = []
+            cont_column = ft.Container()
+            cont_column.content = columu_name
+            cont_column.width = row_button_width + column_width * len(column_name)
+            cont_list.append(cont_column)
             # 作成行をコンテナに入れる
-            for i in range(1, row_count):
+            for content in detail_row:
                 testCon = ft.Container()
-                testCon.content = testlist[i]
-                testCon.width = (
-                    testlist[i].controls[0].width
-                    + testlist[i].controls[1].width
-                    + testlist[i].controls[2].width
-                    + testlist[i].controls[3].width
-                    + testlist[i].controls[4].width
-                )
-                testCon.data = lambda: i
-                testConlist.append(testCon)
-
+                testCon.content = content
+                testCon.width = row_button_width + row_width * len(column_name)
+                testCon.data = lambda: content.controls[0].data
+                cont_list.append(testCon)
             # Columnを作成し、その中にheader_text、Divider、row1、row2、row3を追加
             self.control = ft.Column(
-                controls=testConlist,
+                controls=cont_list,
                 spacing=0,  # Column内の要素間のスペース
                 # horizontal_alignment=ft.CrossAxisAlignment.CENTER,  # Column内の要素を水平方向中央揃え
                 scroll=ft.ScrollMode.ALWAYS,
                 expand=True,
             )
 
-        def on_child_click(page, e, parent_container: ft.Container, color: str):
+        def on_child_click(page, e, parent_container: ft.Container):
             """
             子コントロールがクリックされた時のイベントハンドラ
-            parent_container: 背景色を変更したい親コンテナのインスタンス
-            color: 変更したい色
+            page: ページ
+            e: コントロールイベント
+            parent_container: 親コンテナリスト
             """
             int1 = e.control.data
             for item in parent_container:
                 item.bgcolor = ft.Colors.WHITE
-            parent_container[int1].bgcolor = color
-            # for item in parent_container:
-            #    page.update()
-            # parent_container[int1].update()
+            parent_container[int1 + 1].bgcolor = ft.Colors.GREY
             page.update()
 
-            # クリックされた子コントロールの背景も変える（オプション）
-            e.control.content = ft.Text("赤に変える")
-            e.control.update()
-
-        def page_go(page, e, parent_container: ft.Container, color: str):
+        def page_go(page, e, parent_container: ft.Container):
+            """
+            [詳]ボタンがクリックされた時のイベントハンドラ
+            page: ページ
+            e: コントロールイベント
+            parent_container: 親コンテナリスト
+            """
             int1 = e.control.data
             for item in parent_container:
                 item.bgcolor = ft.Colors.WHITE
-            parent_container[int1].bgcolor = color
+            parent_container[int1 + 1].bgcolor = ft.Colors.GREY
             page.update()
             n = len(page.views) - 1
             page.views[n].data = int1
