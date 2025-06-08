a706f8f70a776ebc64eecfe857e8aa07fef0f8d7
SakumaTakayuki <114669952+SakumaTakayuki@users.noreply.github.com>
2025-06-08 20:09:53 +0900

    【create_table】管理ユーザーのパスワードをハッシュ化

--- db/create_table.py
+++ db/create_table.py
@@ -1,6 +1,7 @@
 from db.models import Bass
 from db.common.engine import Engine
 from db.user_adapter import User_Adapter
+from werkzeug.security import generate_password_hash
 
 
 # テーブルとadminユーザー作成
@@ -21,6 +22,8 @@ class Create_Table(Engine):
         create_row = user_adapter.user_row
         create_row.user_id = self.const.Admin.USER_ID
         create_row.name = self.const.Admin.NAME
-        create_row.password = self.const.Admin.PASSWORD
+        create_row.password = generate_password_hash(
+            self.const.Admin.PASSWORD, method="pbkdf2:sha256"
+        )
         create_row.entry_user_id = self.const.Admin.USER_ID
         user_adapter.create_user(create_row)
