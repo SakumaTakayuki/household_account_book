87cb5b439384673b2dab0e233fd956c2311aaf44
SakumaTakayuki <114669952+SakumaTakayuki@users.noreply.github.com>
2025-06-08 20:03:16 +0900

    【login_ldapter、login】ログイン画面作成

--- db/login_adapter.py
+++ db/login_adapter.py
@@ -1,91 +1,131 @@
 from sqlalchemy import select
 from db.models import User
 from db.common.engine import Engine, Return_Info
+from werkzeug.security import check_password_hash
 
 
 # ログイン用ユーザー情報テーブル接続
 class Login_Adapter(Engine):
     def __init__(self):
         super().__init__()
-        self.user_row = User()
-        self.display = self.const.display.LOGIN
+        # 画面名を設定
+        self.display_name = self.const.display.LOGIN
 
     # ログイン認証
-    def login(self, arg_fill_kbn, arg_where_user_id, arg_password, arg_fill_user_id):
+    def login(self, arg_where_user_id, arg_password, arg_fill_user_id):
+        """
+        ログイン認証を実行する
+        引数
+            arg_fill_kbn：取得区分
+            arg_where_user_id：検索条件
+            arg_password：パスワード
+            arg_fill_user_id：操作者ユーザーID
+        """
+        # 返却用クラスをインスタンス化
         return_user = Return_Info()
+        # ログ出力のfunction_idを作成
         str_log_function_id = self.message.Log_Function_Id.id.format(
             self.const.Log_Kinds.INFO,
             self.const.Log_Process.SELECT,
             self.const.Log_Function.USERS,
         )
-        if arg_fill_kbn == self.const.Fill_Kbn.FIRST:
-            fill_kbn = arg_where_user_id
-        else:
-            fill_kbn = self.const.Const_Text.LIST
+        # STARTログ出力
         if self.create_log(
             self.const.Log_Kinds.START,
             str_log_function_id,
-            self.message.Log_Message.FILL.format(
-                self.display,
-                self.const.Table_Name.USERS,
-                fill_kbn,
+            self.message.Log_Message.LOGIN.format(
+                arg_where_user_id,
                 self.const.Const_Text.TEXT_BLANK,
             ),
             arg_fill_user_id,
         ):
+            # STARTログ出力失敗の場合、ログ出力時例外エラー用メッセージ作成
             return_user.return_message_box = self.exception_log_exception()
             return return_user
         else:
-            if arg_fill_kbn == self.const.Fill_Kbn.FIRST:
-                stmt = select(User).where(
-                    User.user_id == arg_where_user_id, User.password == arg_password
-                )
-            else:
-                stmt = select(User)
+            # ユーザー情報を取得するSELECT文を作成
+            # Where句に引数の検索条件を設定する
+            stmt = select(User).where(User.user_id == arg_where_user_id)
             try:
+                # SELECT文を全件取得で実行する
                 select_row = self.session.scalars(stmt).all()
+                # 取得結果：select_rowが0件の場合
                 if len(select_row) == 0:
+                    # 返却用のメッセージボックスにIDとメッセージ内容を代入する
                     return_user.return_message_box.message_id = "HAB004C"
                     return_user.return_message_box.message_text = (
                         self.message.Message_Box.HAB004C.format(
                             self.const.display.LOGIN
                         )
                     )
+                    # ENDログ出力
                     if self.create_log(
                         self.const.Log_Kinds.END,
                         str_log_function_id,
-                        self.message.Log_Message.Fill_NO_ROW.format(
-                            self.display,
-                            self.const.Table_Name.USERS,
-                            fill_kbn,
+                        self.message.Log_Message.LOGIN_ERROR.format(
+                            arg_where_user_id,
+                            len(select_row),
                         ),
                         arg_fill_user_id,
                     ):
+                        # ENDログ出力失敗の場合、ログ出力時例外エラー用メッセージ作成
                         return_user.return_message_box = self.exception_log_exception()
                         return return_user
                     else:
                         return return_user
                 else:
-                    return_user.return_row = select_row
-                    if self.create_log(
-                        self.const.Log_Kinds.END,
-                        str_log_function_id,
-                        self.message.Log_Message.FILL.format(
-                            self.display,
-                            self.const.Table_Name.USERS,
-                            fill_kbn,
-                            len(select_row),
-                        ),
-                        arg_fill_user_id,
-                    ):
-                        return_user.return_row = None
-                        return_user.return_message_box = self.exception_log_exception()
+                    # 取得したユーザー情報のハッシュ化されたパスワードと引数パスワードが一致する場合
+                    if check_password_hash(select_row[0].password, arg_password):
+                        # 返却用の返却行に取得したユーザー情報を代入する
+                        return_user.return_row = select_row
+                        # ENDログ出力
+                        if self.create_log(
+                            self.const.Log_Kinds.END,
+                            str_log_function_id,
+                            self.message.Log_Message.LOGIN_SUCCESS.format(
+                                arg_where_user_id,
+                                len(select_row),
+                            ),
+                            arg_fill_user_id,
+                        ):
+                            # ENDログ出力失敗の場合、ログ出力時例外エラー用メッセージ作成
+                            return_user.return_row = None
+                            return_user.return_message_box = (
+                                self.exception_log_exception()
+                            )
+                            return return_user
                         return return_user
-                    return return_user
+                    else:
+                        # 返却用のメッセージボックスにIDとメッセージ内容を代入する
+                        return_user.return_message_box.message_id = "HAB004C"
+                        return_user.return_message_box.message_text = (
+                            self.message.Message_Box.HAB004C.format(
+                                self.const.display.LOGIN
+                            )
+                        )
+                        # ENDログ出力
+                        if self.create_log(
+                            self.const.Log_Kinds.END,
+                            str_log_function_id,
+                            self.message.Log_Message.LOGIN_ERROR.format(
+                                arg_where_user_id,
+                                len(select_row),
+                            ),
+                            arg_fill_user_id,
+                        ):
+                            # ENDログ出力失敗の場合、ログ出力時例外エラー用メッセージ作成
+                            return_user.return_message_box = (
+                                self.exception_log_exception()
+                            )
+                            return return_user
+                        else:
+                            return return_user
             except Exception as e:
+                # 例外エラー用メッセージ作成
                 return_user.return_message_box = self.exception_log(
                     str_log_function_id, e, arg_fill_user_id
                 )
                 return return_user
             finally:
+                # セッションを閉じる
                 self.session.close()
--- display/login.py
+++ display/login.py
@@ -3,9 +3,12 @@ from db.login_adapter import Login_Adapter
 from common.my_control import My_Control
 
 
-class Login(ft.View):
-    def __init__(self):
-        self.user_adapter = Login_Adapter()
+# ログイン画面
+class Login(My_Control.MyView):
+    def __init__(self, arg_page: ft.Page):
+        # ログインアダプターをインスタンス化
+        self.login_adapter = Login_Adapter()
+        # 画面ラベル作成
         self.display_label = ft.Container(
             content=ft.Text(
                 "ログイン",
@@ -14,20 +17,32 @@ class Login(ft.View):
             ),
             alignment=ft.alignment.center,
         )
+        # ユーザーID入力エリア作成
         self.id_TextField = ft.TextField(
-            label=ft.Text("ユーザーID", size=18), bgcolor=ft.Colors.WHITE
+            label=ft.Text("ユーザーID", size=18), bgcolor=ft.Colors.WHITE, text_size=18
         )
+        # パスワード入力エリア作成
+        # password=Trueで入力した値がマスクされるように設定
         self.password_TextField = ft.TextField(
-            label=ft.Text("パスワード", size=18), password=True, bgcolor=ft.Colors.WHITE
+            label=ft.Text("パスワード", size=18),
+            password=True,
+            bgcolor=ft.Colors.WHITE,
+            text_size=18,
         )
+        # ログインボタン作成
+        # ボタンクリック時にlogin_submit_clickを呼び出す
         self.submit = ft.FilledButton(
             content=ft.Text("ログイン", size=18),
             width=100,
             height=40,
             disabled=False,
-            on_click=lambda e: Login.login_submit_click(self, e),
+            on_click=lambda e: Login.login_submit_click(self),
         )
+        # controlに作成したコントロールを追加する
+        # ユーザーID入力エリア、パスワード入力エリア、ログインボタンは縦並びかつ枠線内にあるデザインにするため
+        # ft.Container内にft.Column(縦並び)で配置し、ft.Containerに枠線を設定する
         control = [
+            self.display_label,
             ft.Container(
                 content=ft.Column(
                     controls=[
@@ -38,40 +53,47 @@ class Login(ft.View):
                 ),
                 border=ft.border.all(2.0, ft.Colors.BLACK),
                 padding=20,
-            )
+            ),
         ]
+        arg_page.window.width = 505
+        arg_page.window.height = 320
+        arg_page.window.center()
+        # "/login"が呼び出された時にcontrolが表示されるように設定
         super().__init__("/login", control)
 
-    def login_submit_click(self, arg_e):
-        id = self.controls[0].content.controls[0].value
-        password = self.controls[0].content.controls[1].value
-        user_row = self.user_adapter.login(
-            self.user_adapter.const.Fill_Kbn.FIRST,
+    def login_submit_click(self):
+        """
+        ログインボタンクリックイベント
+        """
+        # 画面を非活性にする
+        self.disabled = True
+        self.update()
+        # idにユーザーIDを代入する
+        id = self.controls[1].content.controls[0].value
+        # passwordにパスワードを代入する
+        password = self.controls[1].content.controls[1].value
+        # ログインアダプターを使用し、ログイン認証を行う
+        # user_rowにはユーザー情報、ログイン失敗メッセージが代入されている
+        user_row = self.login_adapter.login(
             id,
             password,
-            self.user_adapter.const.Login.USER_ID,
+            self.login_adapter.const.Login.USER_ID,
         )
+        # user_row.return_rowにユーザー情報がない場合
         if user_row.return_row is None:
+            # user_row.return_message_boxに代入されたメッセージ情報を
+            # 自作コントロールのメッセージボックスに渡しインスタンス化
             msg = My_Control.Msgbox(
                 user_row.return_message_box.message_id,
                 user_row.return_message_box.message_text,
             )
+            # 自作コントロールのメッセージボックスをログイン画面上に表示する
             self.page.open(msg)
+            # 画面を活性にする
+            self.disabled = False
+            self.update()
         else:
-            arg_e.page.go("/HAB_list")
-
-
-def main(page: ft.Page):
-    t = Login(page)
-    page.title = "家計簿アプリ"
-    page.window.width = 505
-    page.window.height = 315
-    page.window.min_width = 500
-    page.window.min_height = 275
-    page.bgcolor = ft.Colors.LIGHT_BLUE_50
-    page.window.center()
-    page.add(t.display_label, t.control)
-
-
-if __name__ == "__main__":
-    ft.app(target=main)
+            # self.page.dataにユーザー情報を代入し、どの画面でもユーザー情報を参照できるようにする
+            self.page.data = user_row.return_row
+            # 家計簿一覧画面を表示する
+            self.page.go("/HAB_list")
